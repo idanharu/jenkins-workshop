dc4051ec1fe3643be40c2e84b11d0c8d112e9259
Idan Harush <iharush@yahoo.com>
2018-09-06 14:12:40 +0300

    add test items

--- src/test/java/net/praqma/codeacademy/gildedrose/TexttestFixture.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/TexttestFixture.java
@@ -10,6 +10,7 @@ public class TexttestFixture {
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Harta Barta, Leg of Flipnipos", 0, 70),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
